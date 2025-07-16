a54cd6438c0c6acea5eab6c74e6f8e27d074d8dd
navid <navidrahman92@gmail.com>
2025-07-16 14:37:27 +0600

    feat: Update main export file with all package components

--- lib/flutter_onboarding_flow.dart
+++ lib/flutter_onboarding_flow.dart
@@ -1,5 +1,12 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library flutter_onboarding_flow;
+
+export 'src/models/onboarding_page_model.dart';
+export 'src/models/onboarding_config.dart';
+export 'src/widgets/onboarding_page_widget.dart';
+export 'src/widgets/onboarding_flow.dart';
+export 'src/widgets/progress_indicator_widget.dart';
+export 'src/widgets/navigation_controls.dart';
+export 'src/controllers/onboarding_controller.dart';
+export 'src/services/onboarding_storage.dart';
+export 'src/templates/onboarding_templates.dart';
+export 'src/animations/page_transitions.dart';
